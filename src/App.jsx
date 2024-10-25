74c7d5dd63ddd456ea538e5aaef115d0a7e03c90
faizannasim <faizannasim59@gmail.com>
2024-10-25 21:57:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
+//import Navbarr from 'Component/Navbarr';
 import './App.css';
 import React from 'react'
-import Navbar from './Component/Navbar'
-import Hero from './Component/Hero';
-import About from './Component/About';
-import Service from './Component/Service';
-import Project from './Component/Project';
-import Contact from './Component/Contact';
-import Footer from './Component/Footer';
+import First from './Component/First';
+import Sec from './Component/Sec';
+import Footerr from './Component/Footerr'
+
+//import Navbar from './Component/Navbar'
+//import Hero from './Component/Hero';
+//import About from './Component/About';
+//import Service from './Component/Service';
+//import Project from './Component/Project';
+//import Contact from './Component/Contact';
+//import Footer from './Component/Footer';
+
+
+
 
 
 
@@ -15,16 +23,11 @@ import Footer from './Component/Footer';
 const App = () => {
   return (
     <div>
-     <Navbar />
-     <Hero />
-     <About />
-     <Hero />
-     <Service />
-     <Project />
-     <Contact />
-     <Footer />
-
+      <First />
+      <Sec />
+      <Footerr />
     </div>
+
 
   )
 }
